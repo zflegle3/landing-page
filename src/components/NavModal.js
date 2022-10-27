03f28ed0bc302bdb2617ef6a679a1094a2f6997a
Zachary Flegle <zflegle3@gmail.com>
2022-10-27 17:21:08 -0400

    Completed mobile nav styling & functionality

--- src/components/NavModal.js
+++ src/components/NavModal.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 
 import { ReactComponent as LogoSvg } from '../images/logo-bookmark.svg';
 import { ReactComponent as CloseSvg } from '../images/icon-close.svg';
+import { ReactComponent as FacebookSvg } from '../images/icon-facebook.svg';
+import { ReactComponent as TwitterSvg } from '../images/icon-twitter.svg';
 
 function NavModal(props) {
     //props.open
@@ -13,14 +15,23 @@ function NavModal(props) {
     return ReactDom.createPortal(
         <>
             <div className="nav-modal">
-                <header>
+                <div className="modal-header">
                     <LogoSvg />
-
                     <button onClick={props.closeNav}>
                         <CloseSvg />
                     </button>
-                </header>
+                </div>
+                <div className="modal-links">
+                    <p onClick={props.closeNav}>FEATURES</p>
+                    <p onClick={props.closeNav}>PRICING</p>
+                    <p onClick={props.closeNav}>CONTACT</p>
+                    <p onClick={props.closeNav} className="selected">LOGIN</p>
+                </div>
 
+                <div className="modal-footer">
+                    <FacebookSvg />
+                    <TwitterSvg />
+                </div>
             </div>
         </>,
         document.getElementById("portal")
