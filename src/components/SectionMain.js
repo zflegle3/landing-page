46b47d004b7e3d3a9a0c81d96a9626b96112b0fc
Zachary Flegle <zflegle3@gmail.com>
2022-10-27 23:37:07 -0400

    completed main section and mobile styling

--- src/components/SectionMain.js
+++ src/components/SectionMain.js
@@ -5,18 +5,21 @@ import { ReactComponent as ContentSvg } from '../images/illustration-hero.svg';
 function SectionMain() {
 
 
+    let vw = window.screen.width;
     return (
       <div className="container-main">
         <div className="container-sub-a">
             <h1>A Simple Bookmark Manager</h1>
-            <p>A clean and simple interface to organize yout favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
+            <p>A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
             <div className="button-container">
-                <button>Get it on Chrome</button>
-                <button>Get it on Firefox</button>
+                <button className="btn-a">Get it on Chrome</button>
+                <button className="btn-b">Get it on Firefox</button>
             </div>
         </div>
         <div className="container-sub-b">
-            <ContentSvg/>
+            {/* <ContentSvg/> */}
+            <div className="svg-hero"></div>
+            <div className="background-right"></div>
         </div>
       </div>
     );
