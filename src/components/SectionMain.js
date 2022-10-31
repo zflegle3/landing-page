d2eaed63b0279982d6c15634587d7ff839dea1b7
Zachary Flegle <zflegle3@gmail.com>
2022-10-31 16:42:06 -0400

    Refactored mobile styling to use mixins

--- src/components/SectionMain.js
+++ src/components/SectionMain.js
@@ -2,7 +2,7 @@ function SectionRight() {
 
     return (
       <div className="container-main">
-        <div className="container-sub-a">
+        <div className="container-main-a">
             <h1>A Simple Bookmark Manager</h1>
             <p>A clean and simple interface to organize your favorite websites. Open a new browser tab and see your sites load instantly. Try it for free.</p>
             <div className="button-container">
@@ -10,7 +10,7 @@ function SectionRight() {
                 <button className="btn-b">Get it on Firefox</button>
             </div>
         </div>
-        <div className="container-sub-b">
+        <div className="container-main-b">
             <div className="svg-hero"></div>
             <div className="background-right"></div>
         </div>
